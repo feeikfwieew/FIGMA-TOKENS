11f4a60afe587a9e8b0e8519b4e3015a042e2bf4
Davi Pellenberg <23endoido12@gmail.com>
2025-09-17 11:35:27 -0300

    Initial commit

---
+++ build/js/token.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  ColorGray900: "#121214",
+  VariableCollectionWhite: "#ffffff",
+  VariableCollectionGray200: "#c1c1c4",
+  VariableCollectionGray500: "#50505c",
+  VariableCollectionGray800: "#1c1c1f",
+  VariableCollectionPurple500: "#8b5cf8",
+};
